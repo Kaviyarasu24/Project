aeb823014736af5ea53ec8f771e4eafc13d5da0c
Kaviyarasu P <kaviyarasup24@gmail.com>
2025-02-03 21:27:37 +0530

    speak engine testing failed

--- JARVIS_personal_assistant/jarvis_main.py
+++ JARVIS_personal_assistant/jarvis_main.py
@@ -2,4 +2,11 @@ import pyttsx3
 
 
 engine = pyttsx3.init("sapi5")
-voices = engine.getProperty("")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+
+engine.setProperty("rate",170)
+
+def wishMe():
+    engine.say("Welcome back!")
+    engine.runAndWait()
